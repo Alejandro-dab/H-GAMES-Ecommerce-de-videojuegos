0163383c8d8f2dd30b4342212a26a235e4d3860e
alejandro-dab <alejandro_ab24@hotmail.com>
2026-09-06 19:51:15 -0600

    Login-Backend: index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,30 @@
 //* Importaciones 
-const conexion = require('../backend/bd/bd'); //Importar conexión al pool de conexiones
-const bcrypt = require('bcrypt'); //Estandar para encripat contraseñas 
+const conexion = require('../backend/bd/bd'); //Conexión al pool de conexiones
 const express = require('express'); //Importar framework de Express
 const rutasUsuario = require('../backend/rutas/usuario.rutas'); //usuario.rutas.js 
+
+require('dotenv').config(); //Leer el .env 
+
+//* Inicialización 
+//Creamos la aplicación express
+const app = express(); //El objeto app es el servidor en si 
+//Iniciara con el puerto de entorno(asignado por hosting) o en el puerto 3000
+const PORT = process.env.PORT || 3000; 
+
+//* Middlewares
+// Midleware nativo de Express para leer JSON 
+app.use(express.json()); 
+
+//*Conexión de rutas
+app.use('api/usuarios', rutasUsuario); //Endpoint HTTP
+
+//Prueba inicial 
+app.get('/', (req, res) => {
+    res.send('API de H-Games en línea');
+});
+
+//*Activar servidor
+//Activamos servidor con el puerto elegido y mandamos un mensaje de consola para confirmar
+app.listen(PORT, () => {
+    console.log(`=> Servidor de H-Games en puerto ${PORT}`);
+});
--- backend/rutas/usuario.rutas.js
+++ backend/rutas/usuario.rutas.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const {registrarUsuario} = require('../controladores/usuario.controlador');
 
 //Definición del endpoint para ser llamado por el cliente 
-router.post('/registro', registrarUsuario); //Ruta virtual
+router.post('/registro', registrarUsuario); //Endpoint HTTP
 
 //Exportación del modulo router para usarlo en index.js 
 //? Al hacer el modulo publico puede ser usado por cualquier archivo que lo importe 
